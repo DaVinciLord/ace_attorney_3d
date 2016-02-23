57ff72eab14334042116600560d6a87e051b341d
TeeGreg <gregoire.pommier@etu.univ-rouen.fr>
2016-02-23 09:56:08 +0100

    essai de lecture des QCM

--- scriptreading.c
+++ scriptreading.c
@@ -2,32 +2,49 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
 #define SCRIPT_PATH "scriptPR.txt"
 
-
+   int traitementdialogue(char buf[]);
 int main() {
 
 	FILE *fic = fopen(SCRIPT_PATH, "r");
 	char buf[BUFSIZ];
 	
 	while (fgets(buf, BUFSIZ, fic)) {
-		int i = 1;
-		if (buf[0] == '-') {
-			while (buf[i] != ' ') {
-				printf("%c", buf[i]);
-				i++;
-			}
-			printf(" dit :");
+		int i = 0;
+		
+		/*if (buf[0] == '*' && buf[1] != '*') {
+			while (buf[i+1] != '*') {
+			printf("%c", buf[i+1]);
+			i++;
 		}
-		while (buf[i] != '\n') {
+		}*/
+		
+		if (buf[0] == '-') {
+			i = traitementdialogue(buf);
+
+		}
+		
+		while (buf[i] != '\0') {
 			printf("%c", buf[i]);
 			i++;
 		}
 		
 			
 		
-		printf("%s", buf);
+		
 		
 	}
 	return EXIT_SUCCESS;
 }
+
+int traitementdialogue(char buf[]) {
+	int i = 1;
+	while (buf[i] != '\n') {
+		printf("%c", buf[i]);
+		i++;
+	}
+	printf(" dit :\n");
+	return (i+1);
+ }
